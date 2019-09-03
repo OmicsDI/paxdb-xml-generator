60b8af8f5cf65867333cb717397e29c7c70561d3
Tu Manh Vu <glmanhtu@gmail.com>
2019-09-03 09:30:30 +0100

    Fixed codestyle

--- src/main/java/uk/ac/ebi/ddi/task/paxdbxmlgenerator/PaxdbXmlGeneratorApplication.java
+++ src/main/java/uk/ac/ebi/ddi/task/paxdbxmlgenerator/PaxdbXmlGeneratorApplication.java
@@ -33,73 +33,73 @@ import java.util.concurrent.atomic.AtomicInteger;
 @SpringBootApplication
 public class PaxdbXmlGeneratorApplication implements CommandLineRunner {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PaxdbXmlGeneratorApplication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaxdbXmlGeneratorApplication.class);
 
-	@Autowired
-	private PaxDBService paxDBService;
+    @Autowired
+    private PaxDBService paxDBService;
 
-	@Autowired
-	private PaxDbXmlGeneratorTaskProperties taskProperties;
+    @Autowired
+    private PaxDbXmlGeneratorTaskProperties taskProperties;
 
-	@Autowired
-	private IFileSystem fileSystem;
+    @Autowired
+    private IFileSystem fileSystem;
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaxdbXmlGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaxdbXmlGeneratorApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		fileSystem.cleanDirectory(taskProperties.getOutputDir());
-		List<String> datasetFiles = paxDBService.getDatasetFiles(taskProperties.getVersion());
-		Map<String, String> mapIdentifiers = paxDBService.getIdentifiers();
-		Map<String, String> proteins = paxDBService.getProteins(taskProperties.getVersion());
-		List<Entry> entries = new ArrayList<>();
-		AtomicInteger fileCount = new AtomicInteger(0);
-		for (String file : datasetFiles) {
-			try {
-				PaxDBDataset dataset = PaxDBDatasetReader.readDataset(file);
-				String key = FileUtils.getNameFromInternalZipPath(file);
-				if (mapIdentifiers.containsKey(key)) {
-					dataset.setFullLink(mapIdentifiers.get(key));
-				}
-				dataset.updateIds(proteins);
-				if (dataset.getIdentifier() != null) {
-					entries.add(Transformers.transformAPIDatasetToEntry(dataset));
-				}
-				if (entries.size() % taskProperties.getEntriesPerFile() == 0) {
-					writeDatasetsToFile(entries, datasetFiles.size(), fileCount.getAndIncrement());
-				}
-			} catch (Exception e) {
-				LOGGER.error("Exception occurred when reading file {}, ", file, e);
-			}
-		}
-		writeDatasetsToFile(entries, datasetFiles.size(), fileCount.getAndIncrement());
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        fileSystem.cleanDirectory(taskProperties.getOutputDir());
+        List<String> datasetFiles = paxDBService.getDatasetFiles(taskProperties.getVersion());
+        Map<String, String> mapIdentifiers = paxDBService.getIdentifiers();
+        Map<String, String> proteins = paxDBService.getProteins(taskProperties.getVersion());
+        List<Entry> entries = new ArrayList<>();
+        AtomicInteger fileCount = new AtomicInteger(0);
+        for (String file : datasetFiles) {
+            try {
+                PaxDBDataset dataset = PaxDBDatasetReader.readDataset(file);
+                String key = FileUtils.getNameFromInternalZipPath(file);
+                if (mapIdentifiers.containsKey(key)) {
+                    dataset.setFullLink(mapIdentifiers.get(key));
+                }
+                dataset.updateIds(proteins);
+                if (dataset.getIdentifier() != null) {
+                    entries.add(Transformers.transformAPIDatasetToEntry(dataset));
+                }
+                if (entries.size() % taskProperties.getEntriesPerFile() == 0) {
+                    writeDatasetsToFile(entries, datasetFiles.size(), fileCount.getAndIncrement());
+                }
+            } catch (Exception e) {
+                LOGGER.error("Exception occurred when reading file {}, ", file, e);
+            }
+        }
+        writeDatasetsToFile(entries, datasetFiles.size(), fileCount.getAndIncrement());
+    }
 
-	private void writeDatasetsToFile(List<Entry> entries, int total, int fileCount) throws IOException {
-		if (entries.size() < 1) {
-			return;
-		}
+    private void writeDatasetsToFile(List<Entry> entries, int total, int fileCount) throws IOException {
+        if (entries.size() < 1) {
+            return;
+        }
 
-		String releaseDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        String releaseDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
 
-		ConvertibleOutputStream outputStream = new ConvertibleOutputStream();
-		try (Writer w = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
-			OmicsDataMarshaller mm = new OmicsDataMarshaller();
+        ConvertibleOutputStream outputStream = new ConvertibleOutputStream();
+        try (Writer w = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+            OmicsDataMarshaller mm = new OmicsDataMarshaller();
 
-			Database database = new Database();
-			database.setDescription(Constants.PAXDB_DESCRIPTION);
-			database.setName(Constants.PAXDB);
-			database.setRelease(releaseDate);
-			database.setEntries(entries);
-			database.setEntryCount(total);
-			mm.marshall(database, w);
-		}
+            Database database = new Database();
+            database.setDescription(Constants.PAXDB_DESCRIPTION);
+            database.setName(Constants.PAXDB);
+            database.setRelease(releaseDate);
+            database.setEntries(entries);
+            database.setEntryCount(total);
+            mm.marshall(database, w);
+        }
 
-		String filePath = taskProperties.getOutputDir() + "/" + taskProperties.getPrefix() + fileCount + ".xml";
-		LOGGER.info("Attempting to write data to {}", filePath);
-		fileSystem.saveFile(outputStream, filePath);
-		entries.clear();
-	}
+        String filePath = taskProperties.getOutputDir() + "/" + taskProperties.getPrefix() + fileCount + ".xml";
+        LOGGER.info("Attempting to write data to {}", filePath);
+        fileSystem.saveFile(outputStream, filePath);
+        entries.clear();
+    }
 }
